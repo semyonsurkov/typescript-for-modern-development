a783ae5e96a93705c2ab04cc5d80b93afe44b927
semyonsurkov <semyonsurkov@outlook.com>
2023-11-21 01:32:05 +0300

    LESSON 12

---
+++ src/05-arrays.ts
@@ -0,0 +1,17 @@
+const numbers = [1, 2, 3, 4];
+
+const strs: string[] = [];
+const strs2: Array<string> = [];
+
+strs.push('asv');
+
+interface Car {
+  wheels: number;
+  brand: string;
+}
+
+const cars: Car[] = [];
+cars.push({ brand: 'Audi', wheels: 3, type: '' });
+
+const arrOfArr: string[][] = [];
+arrOfArr.push(['', '']);
