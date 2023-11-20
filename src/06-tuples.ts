fac92d40dc21d5ba9febaf327c34ae774d9d55bb
semyonsurkov <semyonsurkov@outlook.com>
2023-11-21 01:38:35 +0300

    LESSON 13

---
+++ src/06-tuples.ts
@@ -0,0 +1,10 @@
+const pairs: [string, string][] = [
+  ['key', 'value'],
+  ['key', 'value'],
+];
+
+const data: [number, boolean, string] = [1, true, 'lodash'];
+
+// csv
+const doc: [string, string, number, Date][] = [];
+doc.push(['Semyon', 'Surkov', 12, new Date(1984, 4, 8)]);
