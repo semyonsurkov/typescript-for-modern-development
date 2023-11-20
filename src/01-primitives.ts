2bfba76a0dda13903b513e502ada0c74ab52b4f7
semyonsurkov <semyonsurkov@outlook.com>
2023-11-21 00:58:04 +0300

    LESSON 8

---
+++ src/01-primitives.ts
@@ -0,0 +1,28 @@
+// numbers
+let x = 10.5;
+let y = 1234n;
+let z = NaN;
+
+// string
+let str1 = 'Hello world';
+let symb = Symbol('as');
+
+// boolean
+let o = true;
+o = false;
+
+// nothing
+let h: undefined = undefined;
+let g: null = null;
+
+// literal
+const num = 108;
+const str2 = 'str';
+
+// universal
+let mn: any = 1;
+nm = 'str';
+mm = [];
+
+let xx: unknown = 2;
+if (typeof xx === 'string') xx.toUpperCase();
